b6ecd51cffec7c483e5eb683add199e2d8506384
Satyarth Agrahari <satylogin@gmail.com>
2021-08-15 16:48:22 +0530

    added config driven rss parsing. this would help to add fetching unread ones.

---
+++ src/config.rs
@@ -0,0 +1,17 @@
+use chrono::{DateTime, Utc};
+use serde::{Deserialize, Serialize};
+use std::error::Error;
+use std::fs;
+
+const CONFIG_PATH: &str = "data/config.json";
+
+#[derive(Serialize, Deserialize, Debug)]
+pub(crate) struct Config {
+    pub(crate) feed: String,
+    pub(crate) updated: Option<DateTime<Utc>>,
+}
+
+pub(crate) fn feed_config() -> Result<Vec<Config>, Box<dyn Error>> {
+    let config = fs::read_to_string(CONFIG_PATH)?;
+    Ok(serde_json::from_str(config.as_str())?)
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+pub(crate) mod config;
+
+use config::Config;
 use rss::Channel;
 use std::collections::HashMap;
 use std::error::Error;
@@ -19,15 +22,12 @@ async fn new_feeds(url: &str) -> Result<Vec<String>, Box<dyn Error>> {
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let feeds: Vec<&str> = vec![
-        "https://satylogin.medium.com/feed",
-        "https://dev.to/feed/satylogin",
-    ];
+    let configs: Vec<Config> = config::feed_config()?;
 
     let mut feeds_to_read = HashMap::new();
-    for feed in feeds {
-        let new_feeds = new_feeds(feed).await?;
-        feeds_to_read.insert(feed, new_feeds);
+    for config in configs {
+        let new_feeds = new_feeds(&config.feed).await?;
+        feeds_to_read.insert(config.feed, new_feeds);
     }
     dbg!(feeds_to_read);
 
